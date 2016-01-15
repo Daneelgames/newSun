599e481ac268a24447996338479af1b87e9523a2
Daniil Ermakov <d.ermakov@corpwebgames.com>
2016-01-15 16:58:45 +0300

    Laser fix

---
+++ New Unity Project_oldTextures/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (LineRenderer))]
+
+public class LaserScript : MonoBehaviour {
+	private LineRenderer lr;
+	private int SolidMask = 8;
+	// Use this for initialization
+	void Start () {
+		lr = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.forward, out hit, SolidMask)) {
+			if (hit.collider) {
+				lr.SetPosition (1, new Vector3 (hit.distance, 0, 0));
+			}
+		}
+		else 
+		{
+			lr.SetPosition(1, new Vector3(100,0,0));
+		}
+	}
+}
